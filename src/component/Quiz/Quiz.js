6095e40458a804387c337ac24c239a5d5e58f500
ismilearefin <arefinismile@gmail.com>
2022-10-11 03:50:15 +0600

    Topics section

--- src/component/Quiz/Quiz.js
+++ src/component/Quiz/Quiz.js
@@ -1,15 +1,18 @@
 // import React, { createContext } from 'react';
 import { useLoaderData } from 'react-router-dom';
 
+import './Quiz.css'
+
 
 const Quiz = () => {
     const singleQuiz = useLoaderData();
     const questions = singleQuiz.data.questions
     // console.log(questions)
     return (
-        <div className='flex justify-center'>
-            <div className='mx-2 lg:w-2/4'>
-                {singleQuiz.data.name}
+        <div className='flex justify-center my-8 '>
+            <div className='mx-2 lg:w-2/4 '>
+                <p className='text-2xl font-bold text-blue-800 my-4'>Quiz of {singleQuiz.data.name}</p>
+                <hr/>
                 {
                     questions.map(question => <Question question={question}></Question>)
                 }
@@ -26,18 +29,21 @@ function Question({question}){
     // console.log(rightAns);
     function setAns(x){
         if(x === rightAns){
+            
             alert('right Ans')
+            
         }else{
             alert('Wrong Ans')
         }
     }
 
     return(
-    <div className='border my-4 lg:h-1/6 flex flex-col justify-between drop-shadow-md'>
+    <div className=' my-4 lg:h-1/6 flex flex-col justify-between shadow-lg rounded-lg'>
             <div className='p-8 h-2/4 flex justify-center items-center lg:p-0' >
-                <h1>{question.question}</h1>
+                <h1 className='text-base font-bold'>{question.question}</h1>
             </div>
-        <div className='lg:grid grid-cols-2 gap-2 items-center'>
+            
+        <div className='lg:grid grid-cols-2 gap-2 items-center mb-2 '>
             {
                 options.map(option => <Options option={option} setAns={setAns}></Options>)
             }
