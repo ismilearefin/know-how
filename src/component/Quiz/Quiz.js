596d41f7d2c33f92167405a4f0a77ffc15de5464
ismilearefin <arefinismile@gmail.com>
2022-10-11 13:18:13 +0600

    favIcon

--- src/component/Quiz/Quiz.js
+++ src/component/Quiz/Quiz.js
@@ -1,5 +1,8 @@
 // import React, { createContext } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { EyeIcon} from '@heroicons/react/24/solid'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import './Quiz.css'
 
@@ -9,7 +12,9 @@ const Quiz = () => {
     const questions = singleQuiz.data.questions
     // console.log(questions)
     return (
-        <div className='flex justify-center my-8 '>
+        <div>
+            <ToastContainer />
+            <div className='flex justify-center my-8 '>
             <div className='mx-2 lg:w-2/4 '>
                 <p className='text-2xl font-bold text-blue-800 my-4'>Quiz of {singleQuiz.data.name}</p>
                 <hr/>
@@ -17,6 +22,7 @@ const Quiz = () => {
                     questions.map(question => <Question question={question}></Question>)
                 }
             </div>
+        </div>
         </div>
     );
 };
@@ -26,24 +32,33 @@ const Quiz = () => {
 function Question({question}){
     const options = question.options
     const rightAns = question.correctAnswer
+    
     // console.log(rightAns);
     function setAns(x){
         if(x === rightAns){
-            
-            alert('right Ans')
-            
+            toast.success('Right Ans!', {
+                position: toast.POSITION.TOP_CENTER
+            });
         }else{
-            alert('Wrong Ans')
+            toast.error('Wrong Ans !', {
+                position: toast.POSITION.TOP_CENTER
+            });
         }
+    };
+    function eyeIcon(){
+        toast.info(`Right ans is-  ${rightAns}!`, {
+            position: toast.POSITION.TOP_CENTER
+        });
     }
-
     return(
-    <div className=' my-4 lg:h-1/6 flex flex-col justify-between shadow-lg rounded-lg'>
-            <div className='p-8 h-2/4 flex justify-center items-center lg:p-0' >
+    <div className=' my-4 lg:h-1/6 flex flex-col justify-between shadow-lg rounded-lg p-4'>
+        <div className='flex justify-end'>
+                <EyeIcon onClick={eyeIcon} className="h-6 w-6 text-blue-800"/>
+        </div>
+        <div className='p-8 h-2/4 flex justify-center items-center lg:p-0' >
                 <h1 className='text-base font-bold'>{question.question}</h1>
-            </div>
-            
-        <div className='lg:grid grid-cols-2 gap-2 items-center mb-2 '>
+        </div>
+        <div className='lg:grid grid-cols-2 gap-2 items-center  '>
             {
                 options.map(option => <Options option={option} setAns={setAns}></Options>)
             }
